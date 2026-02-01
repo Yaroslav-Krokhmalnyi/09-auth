5988cb253d14b6d6bae9e5f3c8d2f51e42256fe9
Yaroslav Krokhmalnyi <krokhmalniy.code@gmail.com>
2026-02-01 20:29:50 +0200

    Create singin logic

---
+++ app/(auth routes)/sign-in/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+// Libraries
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { login, LoginRequest } from '@/lib/api/clientApi';
+
+// Styles
+import css from './SignInPage.module.css';
+
+const SignIn = () => {
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const formValues = Object.fromEntries(formData) as LoginRequest;
+      const res = await login(formValues);
+      if (res) {
+        router.push('/profile');
+      } else {
+        setError('Invalid email or password');
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError('Oops... some error');
+      }
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <form className={css.form} action={handleSubmit}>
+        <h1 className={css.formTitle}>Sign in</h1>
+
+        <div className={css.formGroup}>
+          <label htmlFor='email'>Email</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type='submit' className={css.submitButton}>
+            Log in
+          </button>
+        </div>
+
+        {error && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
+};
+
+export default SignIn;
--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -23,3 +23,13 @@ export const register = async (data: RegisterRequest) => {
   const res = await nextServer.post<User>('/auth/register', data);
   return res.data;
 };
+
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export const login = async (data: LoginRequest) => {
+  const res = await nextServer.post<User>('/auth/login', data);
+  return res.data;
+};
